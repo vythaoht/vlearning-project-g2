3d5dce5ad2a12a12e1d71afd4d54413c29bce029
Vy <motchiecvee@Little-Vee.local>
2023-05-25 11:13:41 +0700

    update config project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import HomeTemplate from "./Templates/HomeTemplate";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="" element={<HomeTemplate/>}>
+          <Route path="/" element={<HomePage/>}/>
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
